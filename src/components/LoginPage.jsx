0d576971ef202f8e595b715c936e5e5cdc4c7bd9
SAMANYU07 <samanyu1003@gmail.com>
2024-05-30 22:25:37 +0530

    optimized for mobile

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -68,21 +68,21 @@ export default function LoginPage() {
     {/* <span onClick={listSessions} className=' cursor-pointer'>list</span> */}
     {/* <button onClick={handleLogout}>logout</button> */}
       {!haveAccount ?
-        <div className=' w-[400px] h-[400px] flex flex-col items-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-slate-200 rounded-lg shadow-[0_0_10px_0px_gray]'>
+        <div className=' md:w-[500px] w-[360px] h-[400px] flex flex-col items-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-slate-200 rounded-lg shadow-[0_0_10px_0px_gray]'>
           <h1 className='text-[30px] mt-6'>Register</h1>
-          <input type="text" placeholder='Name' value={name} onChange={(event) => setName(event.target.value)} className=' mt-5 h-[40px] rounded-md pl-4 pr-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
-          <input type="text" placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)} className=' mt-5 h-[40px] rounded-md pl-4 pr-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
-          <input type="password" placeholder='Password' value={pass} onChange={(event) => setPass(event.target.value)} className=' mt-5 h-[40px] rounded-md pl-4 pr-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
+          <input type="text" placeholder='Name' value={name} onChange={(event) => setName(event.target.value)} className=' mt-5 h-[40px] rounded-md md:pl-4 md:pr-4 pl-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
+          <input type="text" placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)} className=' mt-5 h-[40px] rounded-md md:pl-4 md:pr-4 pl-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
+          <input type="password" placeholder='Password' value={pass} onChange={(event) => setPass(event.target.value)} className=' mt-5 h-[40px] rounded-md md:pl-4 md:pr-4 pl-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
           <button className=' mt-6 h-[40px] w-[100px] rounded-lg bg-violet-600 text-white hover:scale-105 active:scale-95 transition-[0.2s]' onClick={handleRegister}>Register</button>
           <div className='mt-6'>
             <span className=''>Don't have an account?</span> <span className=' cursor-pointer text-violet-800' onClick={() => setHaveAccount(true)}>Login</span>
           </div>
         </div>
         :
-        <div className=' w-[400px] h-[400px] flex flex-col items-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-slate-200 rounded-lg shadow-[0_0_10px_0px_gray]'>
+        <div className=' md:w-[500px] w-[360px] h-[400px] flex flex-col items-center absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 bg-slate-200 rounded-lg shadow-[0_0_10px_0px_gray]'>
           <h1 className='text-[30px] mt-6'>Login</h1>
-          <input type="text" placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)} className=' mt-5 h-[40px] rounded-md pl-4 pr-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
-          <input type="password" placeholder='Password' value={pass} onChange={(event) => setPass(event.target.value)} className=' mt-5 h-[40px] rounded-md pl-4 pr-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
+          <input type="text" placeholder='Email' value={email} onChange={(event) => setEmail(event.target.value)} className=' mt-5 h-[40px] rounded-md md:pl-4 md:pr-4 pl-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
+          <input type="password" placeholder='Password' value={pass} onChange={(event) => setPass(event.target.value)} className=' mt-5 h-[40px] rounded-md md:pl-4 md:pr-4 pl-4 text-[20px] outline-none focus:border-b-violet-600 border-2 transition-[0.2s]' />
           <button className=' mt-6 h-[40px] w-[100px] rounded-lg bg-violet-600 text-white hover:scale-105 active:scale-95 transition-[0.2s]' onClick={handleLogin}>Login</button>
           <div className='mt-6'>
             <span className=''>Don't have an account?</span> <span className=' cursor-pointer text-violet-800' onClick={() => setHaveAccount(false)}>Register</span>
